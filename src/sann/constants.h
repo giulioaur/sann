1a9b0ead7a901fff98bcce462c3d43b75f973df7
giulioaur <giulioaur@gmail.com>
2018-02-05 22:52:59 +0100

    Edited gitignore and CMakeLists

--- src/sann/constants.h
+++ src/sann/constants.h
@@ -13,9 +13,9 @@
 #include <string>
 
 namespace sann{
-    const std::string BASE_DIR = ""; // The base directory.
-    const std::string FILES_DIR = BASE_DIR + ""; // The files directory
-    const std::string DATA_SET = ""; // The dataset.
+    const std::string BASE_DIR = "/home/doctor-sushi/Documents/C++Projects/sann/"; // The base directory.
+    const std::string FILES_DIR = BASE_DIR + "files/"; // The files directory
+    const std::string DATA_SET = "monk1";
 }
 
 
